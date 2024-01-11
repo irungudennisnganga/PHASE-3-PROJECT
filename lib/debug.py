9e7edad28ce7dc478588b48fe5c8a6c7bef9c78c
irungudennisnganga <irungud220@gmail.com>
2024-01-11 11:35:23 +0300

    made changes on the class and debug file

--- lib/debug.py
+++ lib/debug.py
@@ -24,11 +24,12 @@ def students_name():
 all_methods.add_command(students_name)
 
 @click.command(help="Add new student")
-# @click.option('--name') 
-# @click.option(--"first_name",last_name,email,date_of_admission)
-# @click.argument('first_name','last_name','email','date')
-def new_student():
-    student.create_student('shalom','mutuku' ,'2023-9-18')       
+@click.option('--first_name',required=True,prompt="Enter first name") 
+@click.option('--last_name',required=True,prompt="Enter last name") 
+@click.option('--date_of_admission',required=True, prompt="Enter date of admission")
+
+def new_student(first_name,last_name,date_of_admission):
+    student.create_student(first_name,last_name ,date_of_admission)       
 all_methods.add_command(new_student)
 
 
@@ -39,12 +40,32 @@ def teachers():
     click.echo(teacher.all_teachers())
 all_methods.add_command(teachers)
 
-@click.command()
-def new_teacher():
-    teacher.create_teacher("mark",'munene','programming')  
+@click.command(help='Add new teacher')
+@click.option('--first_name',required=True,prompt="Enter first name")
+@click.option('--last_name',required=True,prompt="Enter last name")
+@click.option('--subject',required=True,prompt="Enter subject")
+
+def new_teacher(first_name,last_name,subject):
+    teacher.create_teacher(first_name,last_name,subject)  
 all_methods.add_command(new_teacher)  
 
 
-  
+
+# CLASS METHODS
+@click.command(help="New class")
+def new_class():
+    classes.create_class('103',2,4)
+all_methods.add_command(new_class)  
+
+@click.command(help="Filter by class")
+def filter_by_class():
+    click.echo(classes.filter_by_class(1))
+
+all_methods.add_command(filter_by_class)
+
+
+
+
+
 if __name__ == '__main__':
     all_methods()
