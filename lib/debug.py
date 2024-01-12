fe9e3d79a8142ec7678ba823f8a66539cfb6024e
irungudennisnganga <irungud220@gmail.com>
2024-01-12 23:20:13 +0300

    modified debug and added some data to the database

--- lib/debug.py
+++ lib/debug.py
@@ -21,18 +21,24 @@ all_methods.add_command(students)
 
 
 @click.command(help="Add new student")
-@click.option('--first_name',required=True,prompt="Enter first name") 
-@click.option('--last_name',required=True,prompt="Enter last name") 
+@click.option('--first_name',type=str,required=True,prompt="Enter first name") 
+@click.option('--last_name',type=str,required=True,prompt="Enter last name") 
 @click.option('--date_of_admission',required=True, prompt="Enter date of admission")
 
 def new_student(first_name,last_name,date_of_admission):
-    student.create_student(first_name,last_name ,date_of_admission)       
+    new_stud={
+        'first_name':first_name,
+        'last_name':last_name,
+        'date_of_admission':date_of_admission
+        
+    }
+    student.create_student(**new_stud)       
 all_methods.add_command(new_student)
 
 @click.command(help="Update student")
-@click.option('--id',required=True,prompt="Enter Student id ") 
-@click.option('--first_name',required=True,prompt="Enter first name ") 
-@click.option('--last_name',required=True,prompt="Enter last name ") 
+@click.option('--id',type=int,required=True,prompt="Enter Student id ") 
+@click.option('--first_name',type=str,required=True,prompt="Enter first name ") 
+@click.option('--last_name',type=str,required=True,prompt="Enter last name ") 
 @click.option('--date_of_admission',required=True, prompt="Enter date of admission ")
 
 def update_student_info(id,first_name,last_name,date_of_admission):
@@ -41,7 +47,7 @@ def update_student_info(id,first_name,last_name,date_of_admission):
 all_methods.add_command(update_student_info)
 
 @click.command(help='Delete student')
-@click.option('--id',required=True,prompt="Enter Student id ") 
+@click.option('--id',type=int,required=True,prompt="Enter Student id ") 
 def del_student(id):
     student.delete_student(id)
 all_methods.add_command(del_student)   
@@ -56,19 +62,19 @@ def teachers():
 all_methods.add_command(teachers)
 
 @click.command(help='Add new teacher')
-@click.option('--first_name',required=True,prompt="Enter first name")
-@click.option('--last_name',required=True,prompt="Enter last name")
-@click.option('--subject',required=True,prompt="Enter subject")
+@click.option('--first_name',type=str,required=True,prompt="Enter first name")
+@click.option('--last_name',type=str,required=True,prompt="Enter last name")
+@click.option('--subject',type=str,required=True,prompt="Enter subject")
 
 def new_teacher(first_name,last_name,subject):
     teacher.create_teacher(first_name,last_name,subject)  
 all_methods.add_command(new_teacher)  
 
 @click.command(help="Update Teacher")
-@click.option('--id',required=True,prompt="Enter Teacher id ") 
-@click.option('--first_name',required=True,prompt="Enter first name ") 
-@click.option('--last_name',required=True,prompt="Enter last name ") 
-@click.option('--subject',required=True, prompt="Enter subject ")
+@click.option('--id',type=int,required=True,prompt="Enter Teacher id ") 
+@click.option('--first_name',type=str,required=True,prompt="Enter first name ") 
+@click.option('--last_name',type=str,required=True,prompt="Enter last name ") 
+@click.option('--subject',type=str,required=True, prompt="Enter subject ")
 
 def update_teacher_info(id,first_name,last_name,subject):
     student.update_student(id,first_name,last_name,subject)
@@ -76,7 +82,7 @@ def update_teacher_info(id,first_name,last_name,subject):
 all_methods.add_command(update_teacher_info)
 
 @click.command(help='Delete Teacher')
-@click.option('--id',required=True,prompt="Enter Teacher id ") 
+@click.option('--id',type=int,required=True,prompt="Enter Teacher id ") 
 def del_teacher(id):
     teacher.delete_teacher(id)
 all_methods.add_command(del_teacher)   
@@ -85,8 +91,16 @@ all_methods.add_command(del_teacher)
 # CLASS METHODS
 
 @click.command(help="New class")
-def new_class():
-    classes.create_class('103',2,4)
+@click.option('--name',type=str,required=True,prompt="Enter Class name ")
+@click.option('--teachers_id',type=int,required=True,prompt="Enter Teacher id ")
+@click.option('--students_id',type=int,required=True,prompt="Enter Student id ")
+def new_class(name,teachers_id,students_id):
+    new={
+        "name":name,
+        "teachers_id":teachers_id,
+        "students_id":students_id
+    }
+    classes.create_class(**new)
 all_methods.add_command(new_class)  
 
 @click.command(help="Filter by class")
@@ -97,10 +111,10 @@ def filter_by_class(name):
 all_methods.add_command(filter_by_class)
 
 @click.command(help="Update Class ")
-@click.option('--id',required=True,prompt="Enter Class id ") 
-@click.option('--name',required=True,prompt="Enter Class name ") 
-@click.option('--teachers_id',required=True,prompt="Enter teachers id ") 
-@click.option('--students_id',required=True,prompt="Enter students id ") 
+@click.option('--id',type=int,required=True,prompt="Enter Class id ") 
+@click.option('--name',type=str,required=True,prompt="Enter Class name ") 
+@click.option('--teachers_id',type=int,required=True,prompt="Enter teachers id ") 
+@click.option('--students_id',type=int,required=True,prompt="Enter students id ") 
 
 
 def update_classes_info(id,name,teachers_id,students_id):
@@ -109,7 +123,7 @@ def update_classes_info(id,name,teachers_id,students_id):
 all_methods.add_command(update_classes_info)
 
 @click.command(help='Delete Class')
-@click.option('--id',required=True,prompt="Enter Class id ") 
+@click.option('--id',type=int,required=True,prompt="Enter Class id ") 
 def del_classes(id):
     classes.delete_class(id)
 all_methods.add_command(del_classes)
